2407375ee1e716ab34e1637f2a749a0c11ab6a6b
Chris <cmsl4y3r@gmail.com>
2021-07-31 12:38:57 -0600

    html in progress

--- books_authors_app/views.py
+++ books_authors_app/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     
-    return HttpResponse('Hello There')
+    return render(request, 'index.html')
+
+def add_book(request):
+    
+    return render(request, 'add_book.html')
+
+def add_author(request):
+    
+    return render(request, 'add_author.html')
